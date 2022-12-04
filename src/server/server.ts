116f25ad9c2968d1b03d5ba033f04e5fbe17fb5f
Gichelle Amon <81938681+ChelleAmon@users.noreply.github.com>
2022-12-03 20:47:16 -0800

    feat(server): import express, setting up basic connection to localhost

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,1 +1,18 @@
-import * as server from './server.js';
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+
+const app  = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get("/", (req,res) => {
+    res.json("Congratulations! Server is running successfully.")
+})
+
+app.listen(PORT, () => {
+    console.log(`Success! Starting localhost http://localhost:${PORT}`)
+})
