69aea46560b32366e5602256005e7f2ceb920b56
Gichelle Amon <81938681+ChelleAmon@users.noreply.github.com>
2022-12-12 20:39:53 -0800

    feat(nodemailer): import nodemailer, create post route to send mail

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import nodeMailer from 'nodemailer';
 
 const app  = express();
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 const PORT = 3000;
@@ -13,6 +14,20 @@ app.get("/", (req,res) => {
     res.send("Congratulations! Server is running successfully.")
 })
 
+app.post("/sendmail", (req,res) => {
+    console.log("request came");
+    let user: string = req.body;
+    sendMail(user, (err: any, info: any) => {
+            if (err) {
+                console.log("Error: ", err);
+                res.status(400);
+                res.send({error: "Failed to send email"})
+            } else {
+                console.log("Email has been sent")
+                res.send(info)
+            }
+        })
+})
 app.listen(PORT, () => {
     console.log(`Success! Starting localhost http://localhost:${PORT}`)
 })
